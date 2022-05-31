0a061de541089db6cc787a2b4ae88dcc940f2ad1
Nicholas Bachand <ngbachand@gmail.com>
2022-05-31 16:00:17 -0700

    starting to combine the two Hs

--- satellite_H.m
+++ satellite_H.m
@@ -1,8 +1,8 @@
-function [H] = satellite_H(true_s, kernel_size)
+function [H] = satellite_H(true_y, kernel_size)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
-n_pixels = numel(true_s);
+n_pixels = numel(true_y);
 n_data = n_pixels/kernel_size;
 
 % initiate H
@@ -10,5 +10,5 @@ H = zeros(n_data,n_pixels);
 
 % fill in h to act as a 1D average pooling layer
 for i = 1:kernel_size
-    H(:,i:i+n_data) = H(:,i:i+n_data)+eye(n_data)./kernel_size;
+    H(:,i:i+n_data-1) = H(:,i:i+n_data-1)+eye(n_data)./kernel_size;
 end
